bc22581a912316ad044b0c681f7d871cafbd2898
agusryank <agoestryan18@gmail.com>
2021-05-15 14:16:18 +0800

    Perubahan Pada Form

--- application/views/adminkos/tambahkos.php
+++ application/views/adminkos/tambahkos.php
@@ -59,8 +59,9 @@
                                         <option value="Kuta Selatan">Kuta Selatan</option>
                                         <option value="Kuta Utara">Kuta Utara</option>
                                         <option value="Kuta">Kuta</option>
-                                        <option value="Kuta Tengah">Kuta Tengah</option>
+                                        <option value="Kuta Tengah">Kuta Mengwi</option>
                                         <option value="Petang">Petang</option>
+                                        <option value="Petang">Abiansemal</option>
                                     </select>
                                 </div>
                             </div>
